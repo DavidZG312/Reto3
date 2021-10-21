d87e7ec4b1163c4b9ae113ddbbf4f5b5736d34ea
David <lsix1202@hotmail.com>
2021-10-20 23:02:52 -0500

    first commit

---
+++ src/main/java/Reto3/Reto3/Interfaz/InterfaceBoat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto3.Reto3.Interfaz;
+
+import Reto3.Reto3.tabla.Boat;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface InterfaceBoat extends CrudRepository<Boat,Integer> {
+    
+}
---
+++ src/main/java/Reto3/Reto3/Interfaz/InterfaceCategoria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto3.Reto3.Interfaz;
+
+import Reto3.Reto3.tabla.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface InterfaceCategoria extends CrudRepository<Categoria,Integer>{
+    
+}
